15129a1dd926fcc8189b66a1b596850842e1bc78
JoJaeHyeon <whwogus947@naver.com>
2023-11-18 23:41:27 +0900

    Controller, Scenes update

---
+++ Assets/Code/Scripts/GameSystem/GameSceneController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HorrorEscapeGame
+{
+    public class GameSceneController : MonoBehaviour
+    {
+        public void NextScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
